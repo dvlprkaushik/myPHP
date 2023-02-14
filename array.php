03355c71c66abd045339bf18bcf8f760dfeff274
dvlpr_kaushik <freakyboyftw@gmail.com>
2023-02-14 20:03:54 +0530

    code changes

--- array.php
+++ array.php
@@ -3,9 +3,18 @@
 //index array
 
 $arr1 = array('car1','car2','car3');
+
+//associate array
+
+$arr2 = array(
+  /*key*/  'name' => 'KAUSHIK', /*value*/
+    'age' => 19,
+    'status' => true,
+);
 echo "<pre>";
 print_r($arr1); //print_r used to print array
+print_r($arr2);
 echo "</pre>";
 
-echo $arr1[0];
+echo $arr1[0]
 ?>
